ca5f2ed029cb5723b764d3944c61f2177657e6d9
Karthikcahry23 <lingojikarthikchary@gmail.com>
2025-04-07 22:04:46 +0530

    small change

--- src/middleware.js
+++ src/middleware.js
@@ -15,5 +15,5 @@ export function middleware(req) {
 }
 
 export const config = {
-    matcher: "/inbox", // Apply middleware to /inbox route
+    matcher: ["/","/inbox"], // Apply middleware to /inbox route
 };
